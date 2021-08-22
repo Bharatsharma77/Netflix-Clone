c7a134f183701874533bfc012b667bb40f4a54d9
Bharatsharma77 <bharatsharma9368@gmail.com>
2021-08-22 23:19:30 +0530

    Netflix clone

---
+++ index.js
@@ -0,0 +1,141 @@
+/* What is Javascript?
+
+- Programming Language
+- It is used under the Browser (Without Nodejs)
+- In addition it process the data 
+Purpose -> Build Dynamic and action code */
+
+//  Varibles
+// it can we varies as 10 into 20
+ 
+
+//  Two ways to var and let creating varibles
+/* var name = "Bharat Sharma";
+name = "BharatSharma";
+
+let name1 = "Elon Musk👀";
+name1 ="Mr Billionaire😎";
+
+console.log(name1); */
+
+// Scope - visibility
+
+// Global scope - variable can be access inside the program ,var is Global scope
+// Local scope - Inside the Boundary
+// Block scope - {} (Independent Boundary) let is Block Scope
+
+// Global Scope 
+/* var name = "Bill Gates";
+
+
+
+// Locol Scope
+function sayName () {
+    var name = "Raju";
+    console.log(name);
+}
+
+sayName();
+console.log(name); */
+
+ /* let myMarks = 100;
+
+// Block space
+if (myMarks === 100) {
+       let myMarks = 500;
+       console.log(myMarks);
+    } else {
+        console.log(myMarks);
+    }
+   
+        console.log(myMarks); */
+
+        // conditional Statements
+        // If else
+        // Switch
+
+// If else
+     /*   let myMarks = 10;
+        // strict equal too - it compare value and data type 
+        if(myMarks === 50) {
+            console.log("YES");
+        }
+
+        else {
+            console.log("Nooooooo");
+        } */
+         
+
+// Switch
+/* let myMarks = 500;
+
+switch (myMarks) {
+    case 100: {
+        console.log("Good");
+        break;
+    }
+
+  case 200: {
+    console.log("Not Good");
+    break;
+}
+ 
+default :
+console.log("Vulnerable");
+    break; 
+   
+
+} */
+
+// for loop
+
+/* for (let i = 1; i < 20; i++) {
+    console.log(i);
+}  */
+ 
+// console.log("------------------");
+ 
+// while loop
+
+/*  let i = 0 ;
+while (i < 20 ); {
+    console.log(i);
+    i++; 
+}  */
+
+// Do while loop
+/*  let i = 0 ;
+do {
+    console.log(i);
+    i++;
+
+} while (i < 5); */
+
+
+
+
+// function
+
+ /* function hello(name) {
+    //parameter
+    console.log(name);
+}
+
+hello("Bharat Sharma"); // Argument */
+
+
+// Arrow Function
+/* const sayname = (name)  =>  {
+    console.log(name);
+}
+  
+sayname("Bharat Sharma"); */
+
+// DOM manipulation (document object model)
+
+// document.querySelector(".nothing").innerHTML = "Class Base Selector";
+// document.getElementById("dev").innerHTML = "JavaScript DOM";
+
+   
+
+// Netflix clone
